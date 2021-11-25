a2aa95d18df42b87fb40b5603dbf6d46d0b42125
TGboy001 <844072343@qq.com>
2021-11-25 11:43:05 +0800

    feat(weChatRobotController): add WeChat robot controller(v0.1)

--- vue-study/src/App.vue
+++ vue-study/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <weChatRobotController></weChatRobotController>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import weChatRobotController from "./components/weChatRobotController";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    weChatRobotController
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  width: 100%;
+  height: 100%;
+  background-color: dimgrey;
 }
 </style>
